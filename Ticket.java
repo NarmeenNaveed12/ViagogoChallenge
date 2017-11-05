c0d795c0aeb64eb60df110d168bec7bbfd7d69c0
NarmeenNaveed12 <narm96_ch@hotmail.com>
2017-11-05 21:20:18 +0000

    Update Ticket.java

--- Ticket.java
+++ Ticket.java
@@ -7,7 +7,9 @@ public class Ticket {
     double price;
     final double MIN = 1, MAX = 100; 
     Random random=new Random();
-  
+  /**
+  1.Generating random numbers for price here
+  **/
     public Ticket(){
         price = ((double)(Math.random() * (MAX + 1 - MIN))) + MIN;
     }
