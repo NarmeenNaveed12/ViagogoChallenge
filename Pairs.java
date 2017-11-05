450450a8a2b2fd7cab397080841c04682a1b399b
NarmeenNaveed12 <narm96_ch@hotmail.com>
2017-11-05 21:38:11 +0000

    Update Pairs.java

--- Pairs.java
+++ Pairs.java
@@ -1,36 +1,27 @@
-public class Pairs<X, Y>  {
-   X x;
-   Y y;
+public class Pairs {
+   int x;
+   int y;
 
-public Pairs(X x, Y y) {
+public Pairs(int x, int y) {
       this.x = x;
       this.y = y;
 }
 
-public void setX(X x){
+public void setX(int x){
    this.x = x; 
 }
 
-public void setY(Y y){ 
+public void setY(int y){ 
   this.y = y; 
 }
 
-public X getX() {
+public int getX() {
   return this.x;
 }
 
-public Y getY() {
+public int getY() {
   return this.y;
 }
-
-public static <X, Y> Pairs<X, Y> make(X x, Y y) { 
-  return new Pairs<X, Y>(x, y);
-}
-
-public int hashCode() {
-  return (x !=null ? x.hashCode() : 0) + 31 * (y != null ? y.hashCode() : 0);
-}
-
 
 public boolean equals(Object o) {
 if (o == null || o.getClass() != this.getClass()) { 
