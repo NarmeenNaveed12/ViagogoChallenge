7cc47894fb74b9f57e3f394c4baee91255b0cde4
NarmeenNaveed12 <narm96_ch@hotmail.com>
2017-11-05 17:51:58 +0000

    Update Pairs.java

--- Pairs.java
+++ Pairs.java
@@ -2,11 +2,10 @@ public class Pairs<X, Y>  {
    X x;
    Y y;
 
-
-   public Pairs(X x, Y y) {
+public Pairs(X x, Y y) {
       this.x = x;
       this.y = y;
-   }
+}
 
 public void setX(X x){
    this.x = x; 
